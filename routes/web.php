94aef20f2c86db5c48eb0606caed490bd1aa1536
dheerajuparkoti <dheeraj.uparkoti.17@gmail.com>
2024-06-17 16:42:48 +0545

    qn 12,18,20,21,22 soln

--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,11 @@ return view('dashboard.index');
 });
 
 
-//if you are using navigation button then comment 
+//if you are using navigation button then we can comment 
 //SECTION-1 and uncomment SECTION-2 code
 
 //=========SECTION-1================
+
 Route::get('/home',function(){
 return view('home');
 });
@@ -23,14 +24,16 @@ return view('about');
 Route::get('/contact',function(){
     return view('contact');
 });
+
 //END SECTION-1 ====================
 
 
-//For navigation button using PageController only.
+//When using navigation button to redirect to other pages then
 //=========SECTION-2================================
-
-// Route::get('/home',[PageController::class,'home'])->name('home');
-// Route::get('/about',[PageController::class,'about'])->name('about');
-// Route::get('/contact',[PageController::class,'contact'])->('contact');
+//NOTE:DONOT comment these three, otherwise website wont displayed 
+//because of controller link inside.
+Route::get('/home',[PageController::class,'home'])->name('home');
+Route::get('/about',[PageController::class,'about'])->name('about');
+Route::get('/contact',[PageController::class,'contact'])->name('contact');
 
 //END SECTION-2 =====================================
